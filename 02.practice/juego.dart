0d43392ce6c4649dabc95f1ad647ed6a2024e448
jingoochoi <jingoo0206@gmail.com>
2023-12-15 09:55:30 +0900

    Update juego.dart

--- 02.practice/juego.dart
+++ 02.practice/juego.dart
@@ -1,11 +1,25 @@
+import 'dart:math';
+
 void main(List<String> args) {
   dynamic life=30;
-  print('life:$life');
+  // print('life:$life');
   dynamic arco=3;//if random number is 0, miss. (1=attack)
-  print('attack:$arco');
+  // print('attack:$arco');
   dynamic dfen=1;
-  print('defence:$dfen');
+  // print('defence:$dfen');
   dynamic crtc=arco*arco;//attack^2
-  print('critical attack:$crtc');
+  // print('critical attack:$crtc');
+  dynamic msgs=['normal attack!','critical attack!!!'];
   // when enemy attacks, enemy's critical attack is random(1/3)
+  int gong=Random().nextInt(2);
+  if (gong==0) {
+    life=life-arco+dfen;
+    print('life:$life');
+    print(msgs[gong]);
+  }
+  if (gong==1) {
+    life=life-crtc+dfen;
+    print('life:$life');
+    print(msgs[gong]);
+  }
 }
