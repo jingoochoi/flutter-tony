8ca9098caf5395df9c74bfbc076e8d9f2511efcb
jingoochoi <jingoo0206@gmail.com>
2023-12-18 17:24:59 +0900

    Update 02.function.dart

--- 02.practice/02.function.dart
+++ 02.practice/02.function.dart
@@ -5,6 +5,14 @@ void main(List<String> args) {
   // -전달값: 데이터형과 함께 var로 표현
   // int fn(int a){return a+100}//example
 
+  // anonymous function
+  // 형식 : (){코드}
+  // 함수명((){코드})-호출->void 함수(Function f){f()}
+
+  // ramda function
+  // 함수형 콘스트로 선언 후 화살표로 리턴코드 작성
+  // 형식 : Function 함수명=(전달명)=>코드
+
   // 다트의 상수 [final,const]
   show('이순신은 누구일까요?');
   show('이순신의 마지막 전투는?${last[2]}${tail[last[2]]}');
@@ -12,7 +20,41 @@ void main(List<String> args) {
   show(make(1));
   show(make(2));
   show('이순신 시리즈 영화 주요 출연자들:${list(acts.toList())}');
+  show('다음 이순신 시리즈가 있다면 추천 배우는 공유다');
+  show('공유의 취미는 ${rcat['공유']?['취미']}다. 주소는 ${rcat['공유']?['주소']}다. 공유의 자동차는 ${rcat['공유']?['자동차']??'없'}다.');
+  jpbt((){print('적선에 방포하여 침몰시켰다.');});
+  show('아직 신에게는 12척의 배가 남아있습니다.->${last[0]}해전');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  // show('아직 신에게는 ${mina()} 남아있습니다.');
+  show('이순신의 동명이인 부하 입부 이순신이 긴장해서 구구단을 외웠다.');
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
+  show(ipbu());
 }
+  int ship=13;
+  Function mina=()=>--ship;
+  int mult=0;
+  Function ipbu=()=>mult+=9;
+  void jpbt(Function bomb){
+    print('나는 일본 배다. 각오햇!!');
+    bomb();
+  }
+  const rcat={
+    '조인성':{'나이':42,'취미':'날기','주소':'아무데나'},
+    '공유':{'나이':45,'취미':'비가오게하기','주소':'공하우스'},
+  };
   final last=['명량','한산','노량'];
   const tail={'명량':'해전','한산':'도 대첩','노량':'해전'};
   final stit={'명량':null,'한산':'드래곤 출현','노량':'죽음의 바다'};
