95f4766eefed16c70f7791eb6b4d969ecf7e5399
jingoochoi <jingoo0206@gmail.com>
2023-12-18 17:57:12 +0900

    Update juego.dart

--- 02.practice/juego.dart
+++ 02.practice/juego.dart
@@ -1,25 +1,47 @@
+// import 'dart:ffi';
 import 'dart:math';
 
 void main(List<String> args) {
+  game();
+}
+  // void game(){
+  // if (gong==0) {
+  //   life=life-arco+dfen;
+  //   print('❤ $life');
+  //   print(msgs[gong]);
+  //   if (life<1) {
+  //     print('GAME SET');
+  //     return;
+  //   }
+  // }
+  // if (gong==1) {
+  //   life=life-crtc+dfen;
+  //   print('❤ $life');
+  //   print(msgs[gong]);
+  //   if (life<1) {
+  //     print('GAME SET');
+  //     return;
+  //   }
+  // }
+  // game();
+  // }
+  void game(){
+    int gong=Random().nextInt(2);
+    if (gong==0) {
+      life=life-arco+dfen;
+    }else life=life-crtc+dfen;
+    print('❤ $life');
+    print(msgs[gong]);
+    if (life<1) {
+      print('GAME SET');
+      return;
+    }
+    game();
+  }
   dynamic life=30;
-  // print('life:$life');
   dynamic arco=3;//if random number is 0, miss. (1=attack)
-  // print('attack:$arco');
   dynamic dfen=1;
-  // print('defence:$dfen');
   dynamic crtc=arco*arco;//attack^2
-  // print('critical attack:$crtc');
   dynamic msgs=['normal attack!','critical attack!!!'];
   // when enemy attacks, enemy's critical attack is random(1/3)
-  int gong=Random().nextInt(2);
-  if (gong==0) {
-    life=life-arco+dfen;
-    print('❤ $life');
-    print(msgs[gong]);
-  }
-  if (gong==1) {
-    life=life-crtc+dfen;
-    print('❤ $life');
-    print(msgs[gong]);
-  }
-}
+  
