95a5be228c02d7667840f86a546e04a61233a7ad
jingoochoi <jingoo0206@gmail.com>
2023-12-18 18:10:15 +0900

    Update juego.dart

--- 02.practice/juego.dart
+++ 02.practice/juego.dart
@@ -27,11 +27,25 @@ void main(List<String> args) {
   // }
   void game(){
     int gong=Random().nextInt(2);
+    int jong=Random().nextInt(3);
     if (gong==0) {
-      life=life-arco+dfen;
-    }else life=life-crtc+dfen;
-    print('❤ $life');
+      juck=juck-arco;
+    }else juck=juck-crtc;
+    print('juck:❤ $juck');
     print(msgs[gong]);
+    if (juck<1) {
+      print('GAME SET');
+      return;
+    }
+    if (jong<2) {
+      life=life-jatk+dfen;
+      print('me:❤ $life');
+      print(msgs[0]);
+    }else {
+      life=life-jcrt+dfen;
+      print('me:❤ $life');
+      print(msgs[1]);
+    }
     if (life<1) {
       print('GAME SET');
       return;
@@ -44,4 +58,6 @@ void main(List<String> args) {
   dynamic crtc=arco*arco;//attack^2
   dynamic msgs=['normal attack!','critical attack!!!'];
   // when enemy attacks, enemy's critical attack is random(1/3)
-  
+  dynamic juck=40;
+  dynamic jatk=3;//juck attack
+  dynamic jcrt=jatk*jatk;
