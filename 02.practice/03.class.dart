2e8d493988f4aab49fb9f86eb361335a4e19b1c6
jingoochoi <jingoo0206@gmail.com>
2023-12-19 17:44:51 +0900

    .

---
+++ 02.practice/03.class.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  // 다트의 클래스
+  // 정의:목적을 가진 프로그램에서 사용하기 위해 구성요소에 해당하는 요인과 함수로 구성된 프로그램 단위체. 특히 클래스 내부의 함수를 메서드, 요인을 속성이라고 한다.
+  // 형식:class Myclass(pascal case) {code}
+}
+class Dogs{
+  // 속성
+  String name='sibasaki';
+  int ages=8;
+  String colo='black';
+  int bite=100;
+  // 메서드
+  void tdog(){
+    bite=bite-5;
+  }
+}
+class Cats{
+  String name='nabi';
+  int ages=5;
+  String colo='polka-dotted';
+  int punc=100;
+  // 메서드
+  void tdog(){
+    punc=punc-5;
+  }
+}
