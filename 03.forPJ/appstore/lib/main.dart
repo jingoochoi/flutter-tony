6c2abc6f9e63a941bfde6ee8fa9ec0201aba6ee1
jingoochoi <jingoo0206@gmail.com>
2023-12-26 16:51:18 +0900

    .

--- 03.forPJ/appstore/lib/main.dart
+++ 03.forPJ/appstore/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: StorePage(),
     );
   }
 }
+
 class StorePage extends StatelessWidget {
   const StorePage({super.key});
 
@@ -23,14 +27,85 @@ class StorePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [Row(children: [
-            Text('Woman',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
-            Text('Kids',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
-            Text('Shoes',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
-            Text('Bags',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
-          ],)],
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Woman',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.green),
+                  ),
+                  Icon(
+                    Icons.woman,
+                    color: Colors.green,
+                  ),
+                  Spacer(),
+                  Text(
+                    'Kids',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.yellow),
+                  ),
+                  Icon(
+                    Icons.child_care,
+                    color: Colors.yellow,
+                  ),
+                  Spacer(),
+                  Text(
+                    'Shoes',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.blue),
+                  ),
+                  Icon(
+                    Icons.shop,
+                    color: Colors.blue,
+                  ),
+                  Spacer(),
+                  Text(
+                    'Bags',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.red),
+                  ),
+                  Icon(
+                    Icons.backpack,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Expanded(
+                child: Image.asset(
+              'bag.jpeg',
+              fit: BoxFit.cover,
+            )),
+            Expanded(
+              child: Image.asset(
+                'cloth.jpeg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            //경로를 야믈파일에 저장했다!!
+          ],
         ),
       ),
     );
   }
 }
+/*
+            [ Padding 위젯 ]
+            padding: EdgeInsets.all(4방향전체),
+            padding: EdgeInsets.only(left/top/right/bottom특정방향만),
+            padding: EdgeInsets.symmetric(수직/수평중),
+           */
